350d1a37c9e25668eb25ee8a2c0b145a8983c990
Shubham Gangwal <shubhamgang123@gmail.com>
2023-12-06 09:31:30 +0530

    add one edit away class file

---
+++ arrays-and-string/src/OneEditAway.java
@@ -0,0 +1,8 @@
+/**
+ * Determine if a string is one edit away. Edit means add, remove or replace a character.
+ */
+public class OneEditAway {
+    public static void main(String[] args) {
+
+    }
+}
